a3bc613ad3303a1a4d87503900ab02c47d5374d4
Kwesi <kwesi@webmagnat.ro>
2024-08-04 08:06:23 +0000

    props change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Car from './Car';
+import Component from './Component';
+
+
 
 function App() {
   return (
     <div className="App">
      <h2>Hello people</h2>
+     <Component name="Sandy"/>
+     <Car/>
+     
     </div>
   );
 }
---
+++ src/Component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Component(props) {
+  return (
+    <div>
+      <h3>Last App is {props.name}</h3>
+    </div>
+  )
+}
+
+export default Component
